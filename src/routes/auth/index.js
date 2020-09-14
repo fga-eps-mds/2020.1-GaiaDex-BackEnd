8771ad4a305160d0481f64dedf03601bbf48f5bd
senaarth <senaarth@gmail.com>
2020-09-14 18:54:18 -0300

    Validate req body (username and password)
    Co-authored-by: Victor Santos <victor.samuelsantoss@gmail.com>

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const Joi = require('Joi');
 
 const router = express.Router();
 
+const schema = Joi.object({
+    username: Joi.string().alphanum().min(4).max(20).required(),
+    password: Joi.string().alphanum().min(8).required()
+});
+
 // Every route ir is preceded by /auth
+
 router.get('/', (req, res) => {
     res.json({
         message: 'Authentication!'
     });
+});
+
+router.post('/signup', (req, res) => {
+    const result = schema.validate(req.body);
+
+    if(result.error) {
+        console.log(result.error);
+        next(result.error);
+    } else {
+
+    }
+
 });
 
 module.exports = router;
