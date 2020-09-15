da94259bbca7e416238ff7850d6658901f9e5405
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2020-09-15 10:07:16 -0300

    add user model
    Co-authored-by: Vinicius Vieira de Souza <vd.souza1999@gmail.com>

---
+++ src/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        require: true,
+    },
+    email:{
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+    },
+    password:{
+        type: String,
+        required: true,
+        select: false,
+    },
+
+});
+
+const User = mongoose.model('User', UserSchema);
