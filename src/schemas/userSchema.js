4709f8435ffda92e3dd38bc2cc3629a2d7bb6f51
senaarth <senaarth@gmail.com>
2020-09-23 20:57:03 -0300

    Add password confirmation
    Co-authored-by: Victor Santos <victor.samuelsantoss@gmail.com>

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -3,6 +3,7 @@ const Joi = require('joi');
 const userSchema = Joi.object({
     username: Joi.string().alphanum().min(4).max(30).required(),
     password: Joi.string().min(8).required(),
+    passwordConfirmation: Joi.string().min(8).required().valid(Joi.ref('password')).error(new Error('Password confirmation does not match.')),
     email: Joi.string().email().required()
 });
 
