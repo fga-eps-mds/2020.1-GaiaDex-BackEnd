3b5df2f54ff98db323c6c36eae0d63947e6b5c00
senaarth <senaarth@gmail.com>
2020-09-24 16:33:04 -0300

    Add comment model and its creation route
    Co-authored-by: Victor Santos <victor.samuelsantoss@gmail.com>

--- src/routes/topicRoutes.js
+++ src/routes/topicRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const Topic = require('../models/topic');
 const User = require('../models/user');
+const Comment = require('../models/comment');
 
 router.post('/create/:userId', async (req, res, next) => {
     
@@ -27,7 +28,7 @@ router.post('/create/:userId', async (req, res, next) => {
 
 });
 
-router.put('/update/:topicId', async (req, res) => {
+router.put('/update/:topicId', async (req, res, next) => {
 
     try {
 
@@ -94,7 +95,7 @@ router.delete('/delete/:topicId', async (req, res) => {
     }
 });
 
-router.get('/list_topics', async (req, res) => {
+router.get('/list_topics', async (req, res, next) => {
     try {
         const topic = await Topic.find().populate(['user']);
 
@@ -104,5 +105,28 @@ router.get('/list_topics', async (req, res) => {
         return res.next({ error: 'Error loading topics.' });
     }
 });
+
+router.post('/comment/:topicId/:userId', async (req, res, next) => {
+    try {
+
+        const comment = await Comment.create({...req.body, user: req.params.userId, topic: req.params.topicId});
+        const user = await User.findById(req.params.userId);
+        const topic = await Topic.findById(req.params.topicId);
+
+        if (!user) return next(new Error('User not found.'));
+        if (!topic) return next(new Error('Topic not found.'));
+
+        await comment.save();
+
+        topic.comments.push(comment);
+        await topic.save();
+    
+        return res.send({ message: 'Comment successfully registered.' });
+
+    } catch (err) {
+        console.log(err);
+        return res.next({ error: 'Error, it was not possible to comment.'});
+    }
+})
 
 module.exports = router;
