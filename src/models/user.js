9cb8250dcafb3b36f7b69e75200382074f1f3d7a
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2020-09-24 01:14:23 -0300

    solving conflicts
    Co-authored-by: Victor Hugo Siqueira Costa <vitao.hugo@hotmail.com>

--- src/models/user.js
+++ src/models/user.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        require: true,
-    },
-    email:{
-        type: String,
-        unique: true,
-        required: true,
-        lowercase: true,
-    },
-    password:{
-        type: String,
-        required: true,
-        select: false,
-    },
+const Schema = mongoose.Schema;
 
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    }
 });
 
-module.exports = User = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,6 +6,8 @@ const jsonwebtoken = require('jsonwebtoken');
 
 const User = require('../models/user');
 
+const userSchema = require('../schemas/userSchema');
+
 router.get('/', (req, res) => {
     res.json({
         message: 'Authentication!'
@@ -80,6 +82,83 @@ router.get('/logout', async(req, res) => {
     });
 
     res.redirect('/login');
+});
+
+router.post('/signup', async(req, res, next) => {
+
+    try {
+
+        const newUserData = req.body;
+        const result = userSchema.validate(req.body);
+
+        if( await User.findOne({ username: newUserData.username}) ) {
+            const error = new Error('Username already being used.');
+            return next(error);
+        }
+
+        if( result.error ) {
+            return next(result.error);
+        }
+
+        const user = new User(newUserData);
+
+        user.save()
+            .then( result => {
+                return res.send(result);
+            })
+            .catch( err => next(err));
+
+    } catch(err) {
+        return next(err);
+    }
+
+});
+
+router.put('/update-user/:id', async(req, res, next) => {
+
+    try {
+
+        const user = await User.findById(req.params.id);
+        const newData = req.body;
+
+        if ( !newData.username ) {
+            newData.username = user.username;
+        }
+        if ( !newData.password ) {
+            newData.password = user.password;
+        }
+        if ( !newData.email ) {
+            newData.email = user.email;
+        }
+
+        const result = userSchema.validate(newData);
+
+        if(result.error) {
+            return next(result.error);
+        }
+        
+        await User.findOneAndUpdate({_id: req.params.id}, req.body, { useFindAndModify: false})
+                    .then( () => {
+                        res.send({ message: 'User updated successfully.'});
+                    });
+
+    } catch(err) {
+        return next(err);
+    }
+
+});
+
+router.delete('/delete-user/:id', async(req, res, next) => {
+    
+    try {
+        
+        await User.findByIdAndDelete(req.params.id);
+        return res.send({ message: 'User successfully deleted.' });
+
+    } catch(err) {
+        return next(err);
+    }
+
 });
 
 module.exports = router;
