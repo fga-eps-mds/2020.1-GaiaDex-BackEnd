d8289708e609e2c961eff2874ff19eaabdbced89
Rafael Makaha <rafael.makaha@gmail.com>
2020-10-16 17:42:14 -0300

    Removes user.js and Update User.js
    Co-authored-by: Antonio Ruan <ruanmoura13@outlook.com>
    Co-authored-by: Marcos Filipe <marofelipe@live.com>

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     username: {
         type: String,
-        required: true
+        required: true,
+        unique:true
     },
     password: {
         type: String,
@@ -12,7 +13,8 @@ const userSchema = new Schema({
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique:true
     },
     topics: [{
         type: mongoose.Schema.Types.ObjectId,
