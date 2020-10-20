524a6f473712210dae81d7455316c11bf35e75c0
Antonio Ruan Moura Barreto <ruanmoura13@outlook.com>
2020-10-20 19:19:32 -0300

    Adding scanner plant rounter
    Co-authored-by: Marcos Felipe De Almeida Souza <marofelipe@live.com>

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const authRoutes = require('./routes/authRoutes');
 const plantRoutes = require('./routes/plantRoutes');
 const topicRoutes = require('./routes/topicRoutes');
 const commentRoutes = require('./routes/commentRoutes');
+const scanner = require('./routes/scanner')
 
 // MongoDB connection
 // mongodb://localhost:27017/noderest  => meu banco de dados local polupado
@@ -18,19 +19,20 @@ mongoose
   .catch((err) => console.log(err));
 
 // middlewares
+app.use(express.json({ limit: 20*1024*1024}));
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // routes
-
-app.use('/auth', authRoutes);
-app.use('/plant', plantRoutes);
-app.use('/topic', topicRoutes);
-app.use('/comment', commentRoutes);
+app.use('/auth',authRoutes);
+app.use('/plant',plantRoutes);
+app.use('/topic',topicRoutes);
+app.use('/comment',commentRoutes);
+app.use('/scanner',scanner);
 
 // starting the server
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 2000);
 app.listen(app.get('port'), () => {
   console.log(`Server on port ${app.get('port')}`);
 });
