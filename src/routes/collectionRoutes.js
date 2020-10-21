181d5da2ce762f3294e87330b2362559d453e253
faco400 <vd.souza1999@gmail.com>
2020-10-21 20:52:02 -0300

    Create collection route and model
    Co-authored-by: Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>

--- src/routes/collectionRoutes.js
+++ src/routes/collectionRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const router = express.Router();
 
 const User = require('../models/User');
@@ -6,60 +7,60 @@ const MyPlant = require('../models/myPlant');
 const Plant = require('../models/Plant');
 const plantNotification = require('../models/plantNotification');
 
-router.get('/', async(req, res) => {
-    res.send({message: "collection"});
+router.get('/', async (req, res) => {
+  res.send({ message: 'collection' });
 });
 
-router.get('/:userId', async(req, res) =>{
-    try{
-        const user = await User.findById(req.params.userId);
-        const length = user.myPlants.length;
-        console.log(user.myPlants.length);
-        if(length > 0) {
-            var plantArray=[];
-            for(var index = 0; index < user.myPlants.length; index++) {
-                var myplant = await MyPlant.findById(user.myPlants[index]);
-                var typePlant = await Plant.findById(myplant.plant);
-                var objplant = "{\"nickname\" : \"" + myplant.nickname + "\", \"commonName\" : \"" + typePlant.commonName + "\", \"profilePicture\" : \"" + typePlant.profilePicture + "\"}";
-                var objplant = JSON.parse(objplant);
-                plantArray.push(objplant);
-            }
+router.get('/:userId', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    const { length } = user.myPlants;
+    console.log(user.myPlants.length);
+    if (length > 0) {
+      const plantArray = [];
+      for (let index = 0; index < user.myPlants.length; index++) {
+        const myplant = await MyPlant.findById(user.myPlants[index]);
+        const typePlant = await Plant.findById(myplant.plant);
+        var objplant = `{"nickname" : "${myplant.nickname}", "commonName" : "${typePlant.commonName}", "profilePicture" : "${typePlant.profilePicture}"}`;
+        var objplant = JSON.parse(objplant);
+        plantArray.push(objplant);
+      }
 
-            res.send(plantArray);
-            //return res.send(user.myPlants[0]);
-            //return res.send({message: "collection!!"})
+      res.send(plantArray);
+      // return res.send(user.myPlants[0]);
+      // return res.send({message: "collection!!"})
 
-            // const data = new Date();
-            // const tempo = {
-            //     "dia": data.getDate(),
-            //     "mes": data.getMonth()+1,
-            //     "ano": data.getFullYear(),
-            // }
-
-        }else{
-            return res.send({message: "No plants in my collection"});
-        }
-    }catch(err){
-        return res.status(400).send({error: 'Error visualizing collection' + err});
+      // const data = new Date();
+      // const tempo = {
+      //     "dia": data.getDate(),
+      //     "mes": data.getMonth()+1,
+      //     "ano": data.getFullYear(),
+      // }
+    } else {
+      return res.send({ message: 'No plants in my collection' });
     }
-})
-router.post('/:myPlantID', async(req,res) =>{
-    try{
-        const {water, fertilize} = req.body
-        const notification = await plantNotification.create(water, fertilize);
-        const myplant = await MyPlant.findById(req.params.myPlantID);
+  } catch (err) {
+    return res
+      .status(400)
+      .send({ error: `Error visualizing collection${err}` });
+  }
+});
+router.post('/:myPlantID', async (req, res) => {
+  try {
+    const { water, fertilize } = req.body;
+    const notification = await plantNotification.create(water, fertilize);
+    const myplant = await MyPlant.findById(req.params.myPlantID);
 
-        await notification.save();
+    await notification.save();
 
-        myplant.notifications.push(notification);
+    myplant.notifications.push(notification);
 
-        await myplant.save();
+    await myplant.save();
 
-        return res.send({ message: 'Notification successfully registered.' });
-
-    }catch(err){
-        return res.status(400).send({error: 'Error adding notification' + err});
-    }
-})
+    return res.send({ message: 'Notification successfully registered.' });
+  } catch (err) {
+    return res.status(400).send({ error: `Error adding notification${err}` });
+  }
+});
 
 module.exports = router;
