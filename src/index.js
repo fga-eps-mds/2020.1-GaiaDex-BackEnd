c1541916e48c2819e0abf55945a0f49af712f52c
Marcos Felipe <marofelipe@live.com>
2020-10-29 23:31:38 -0300

    change port to 3000 at index.js

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ app.use('/comment',commentRoutes);
 app.use('/scanner',scanner);
 
 // starting the server
-app.set('port', process.env.PORT || 2000);
+app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), () => {
   console.log(`Server on port ${app.get('port')}`);
 });
