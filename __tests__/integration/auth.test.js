838809939fd1479421ad99b739e0bbf9e8c2b83e
faco400 <vd.souza1999@gmail.com>
2020-11-01 15:27:13 -0300

    add 4 more tests to auth.test.js, 1 about signup still needs fixing

--- __tests__/integration/auth.test.js
+++ __tests__/integration/auth.test.js
@@ -16,6 +16,55 @@ describe('Auth/User', () => {
 
     expect(response.status).toBe(200);
   });
+  // nao ta funcionando
+  it('should give error because trying to signup user that already exists', async () => {
+    const user = new UserModel({
+      username: 'joca',
+      password: 'azul_porta',
+      passwordConfirmation: 'azul_porta',
+      email: 'jocaEdoido@gmail.com',
+    });
+    await user.save();
+
+    const response = await request.post('/auth/signup').send({
+      username: 'joca',
+      password: 'azul_porta',
+      passwordConfirmation: 'azul_porta',
+      email: 'jocaEdoido@gmail.com',
+    });
+
+    expect(response.request).toBe(400);
+  });
+
+  it('should give error 400 because the signup of username with less of 4 characters', async () => {
+    const response = await request.post('/auth/signup').send({
+      username: 'me',
+      password: 'porta_vermelha',
+      passwordConfirmation: 'porta_vermelha',
+      email: 'joaozao11@gmail.com',
+    });
+    expect(response.status).toBe(400);
+  });
+
+  it('should give error 400 because password not valid (less than 8 char)', async () => {
+    const response = await request.post('/auth/signup').send({
+      username: 'joazin',
+      password: 'oi',
+      passwordConfirmation: 'porta_vermelha',
+      email: 'joaozao11@gmail.com',
+    });
+    expect(response.status).toBe(400);
+  });
+
+  it('should give error 400 because passwordConfirmation wrong', async () => {
+    const response = await request.post('/auth/signup').send({
+      username: 'joazin',
+      password: 'porta_verde',
+      passwordConfirmation: 'porta_azul',
+      email: 'joaozao11@gmail.com',
+    });
+    expect(response.status).toBe(400);
+  });
 
   it('should be able to delete user', async () => {
     const user = new UserModel({
