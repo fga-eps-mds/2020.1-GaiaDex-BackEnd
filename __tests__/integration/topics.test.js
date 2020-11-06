2a75809af6da43de7f03f6fecfcd4a240b2790cd
senaarth <senaarth@gmail.com>
2020-11-06 14:17:11 -0300

    Adds all topic creation tests

--- __tests__/integration/topics.test.js
+++ __tests__/integration/topics.test.js
@@ -4,36 +4,38 @@ const TopicModel = require('../../src/models/Topic');
 const UserModel = require('../../src/models/User');
 const PlantModel = require('../../src/models/Plant');
 
+// Hypotetical variables
+  const user = new UserModel({
+    username: 'username',
+    password: 'password',
+    passwordConfirmation: 'password',
+    email: 'email@email.com',
+  });
+  user.save();
+
+  const plant = new PlantModel({
+    scientificName: 'Butia archeri Glassman',
+    family_name: 'Arecaceae',
+    gender_name: 'Butia',
+    specie_name: 'Butia archeri',
+    common_name : 'butiazinho',
+    usage: 'A espécie é conhecida popularmente como butiazinho, coqueirinho-do-campo, butiá-do-campo, butiá-do-cerrado ou palmeira-butiá. Em alguns locais do Cerrado, as folhas desta palmeirinha são utilizadas para a confecção de vassouras, daí atribui-se também o nome popular de palmeira-de-vassoura.',
+    first_User: ' julceia',
+    collection_count: '108',
+    extinction: '0',
+    profile_picture: 'https://static.inaturalist.org/photos/68945583/large.jpeg?1587849882',
+    gbifID: '28601793778',
+    stateProvince: 'Distrito Federal',
+    topicos: []
+  });
+  plant.save();
+
 const request = supertest(app);
 
 describe('topic/', () => {
+
   // Creation
   it('Should be able to create a new topic.', async () => {
-
-    const user = new UserModel({
-      username: 'username',
-      password: 'password',
-      passwordConfirmation: 'password',
-      email: 'email@email.com',
-    });
-    await user.save();
-
-    const plant = new PlantModel({
-      scientificName: 'Butia archeri Glassman',
-      family_name: 'Arecaceae',
-      gender_name: 'Butia',
-      specie_name: 'Butia archeri',
-      common_name : 'butiazinho',
-      usage: 'A espécie é conhecida popularmente como butiazinho, coqueirinho-do-campo, butiá-do-campo, butiá-do-cerrado ou palmeira-butiá. Em alguns locais do Cerrado, as folhas desta palmeirinha são utilizadas para a confecção de vassouras, daí atribui-se também o nome popular de palmeira-de-vassoura.',
-      first_User: ' julceia',
-      collection_count: '108',
-      extinction: '0',
-      profile_picture: 'https://static.inaturalist.org/photos/68945583/large.jpeg?1587849882',
-      gbifID: '28601793778',
-      stateProvince: 'Distrito Federal',
-      topicos: []
-  });
-    await plant.save();
 
     const response = await request.post(`/topic/create/${plant.id}/${user.id}/`).send({
       title: 'Titulo Tópico',
@@ -42,4 +44,24 @@ describe('topic/', () => {
 
     expect(response.status).toBe(200);
   });
+
+  it('Should not be able to create a new topic because there is no topic title.', async () => {
+
+    const response = await request.post(`/topic/create/${plant.id}/${user.id}/`).send({
+      description: 'Dúvidas sobre planta',
+    });
+
+    expect(response.status).toBe(400);
+  });
+
+  it('Should be able to create a new topic because topic title is too short.', async () => {
+
+    const response = await request.post(`/topic/create/${plant.id}/${user.id}/`).send({
+      title: 'T',
+      description: 'Dúvidas sobre planta',
+    });
+
+    expect(response.status).toBe(400);
+  });
+
 });
