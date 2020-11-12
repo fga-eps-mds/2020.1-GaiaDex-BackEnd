77841a00183f8551be018c65d74446ca3db52faf
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2020-11-12 07:25:31 -0400

    correcting identation

--- __tests__/integration/comments.test.js
+++ __tests__/integration/comments.test.js
@@ -8,44 +8,44 @@ const CommentModel = require('../../src/models/Comment');
 // Hypotetical variables
 
 const user = new UserModel({
-    username: 'username',
-    password: 'password',
-    passwordConfirmation: 'password',
-    email: 'email@email.com',
+  username: 'username',
+  password: 'password',
+  passwordConfirmation: 'password',
+  email: 'email@email.com',
 });
 user.save();
 
 const plant = new PlantModel({
-    scientificName: 'Butia archeri Glassman',
-    family_name: 'Arecaceae',
-    gender_name: 'Butia',
-    specie_name: 'Butia archeri',
-    common_name: 'butiazinho',
-    usage:
-        'A espécie é conhecida popularmente como butiazinho, coqueirinho-do-campo, butiá-do-campo, butiá-do-cerrado ou palmeira-butiá. Em alguns locais do Cerrado, as folhas desta palmeirinha são utilizadas para a confecção de vassouras, daí atribui-se também o nome popular de palmeira-de-vassoura.',
-    first_User: ' julceia',
-    collection_count: '108',
-    extinction: '0',
-    profile_picture:
-        'https://static.inaturalist.org/photos/68945583/large.jpeg?1587849882',
-    gbifID: '28601793778',
-    stateProvince: 'Distrito Federal',
-    topicos: [],
+  scientificName: 'Butia archeri Glassman',
+  family_name: 'Arecaceae',
+	gender_name: 'Butia',
+	specie_name: 'Butia archeri',
+	common_name: 'butiazinho',
+	usage:
+			'A espécie é conhecida popularmente como butiazinho, coqueirinho-do-campo, butiá-do-campo, butiá-do-cerrado ou palmeira-butiá. Em alguns locais do Cerrado, as folhas desta palmeirinha são utilizadas para a confecção de vassouras, daí atribui-se também o nome popular de palmeira-de-vassoura.',
+	first_User: ' julceia',
+	collection_count: '108',
+	extinction: '0',
+	profile_picture:
+			'https://static.inaturalist.org/photos/68945583/large.jpeg?1587849882',
+	gbifID: '28601793778',
+	stateProvince: 'Distrito Federal',
+	topicos: [],
 });
 plant.save();
 
 const topic = new TopicModel({
-    title: 'test',
-    description: 'test',
-    user: user.id,
-    plant: plant.id,
+	title: 'test',
+	description: 'test',
+	user: user.id,
+	plant: plant.id,
 });
 topic.save();
 
 const comment = new CommentModel({
-    text: 'test',   
-    user: user.id,
-    topic: topic.id,
+	text: 'test',   
+	user: user.id,
+	topic: topic.id,
 });
 comment.save();
 
@@ -53,61 +53,61 @@ const request = supertest(app);
 
 describe('comment/', () => {
 
-    it('Should be able to comment because there is text', async() => {
-        const response = await request
-            .post(`/comment/create/${topic.id}/${user.id}`)
-            .send({
-                text: 'Comentario',
-            });
+	it('Should be able to comment because there is text', async() => {
+		const response = await request
+			.post(`/comment/create/${topic.id}/${user.id}`)
+			.send({
+				text: 'Comentario',
+			});
 
-            expect(response.status).toBe(200);
-    });
+		expect(response.status).toBe(200);
+	});
 
-    it('Should not be able to comment because there is no text', async() => {
-        const response = await request
-            .post(`/comment/create/${topic.id}/${user.id}`)
-            .send({
-                text: '',
-            });
+	it('Should not be able to comment because there is no text', async() => {
+		const response = await request
+			.post(`/comment/create/${topic.id}/${user.id}`)
+			.send({
+				text: '',
+			});
 
-            expect(response.status).toBe(400);
-    });
+		expect(response.status).toBe(400);
+	});
 
-    it('Should be able to update the comment because there is text', async() => {
-        const response = await request
-            .put(`/comment/update/${comment.id}`)
-            .send({
-                text: 'Comentario atualizado',
-            });
+	it('Should be able to update the comment because there is text', async() => {
+		const response = await request
+			.put(`/comment/update/${comment.id}`)
+			.send({
+				text: 'Comentario atualizado',
+			});
 
-            expect(response.status).toBe(200);
-    });
+		expect(response.status).toBe(200);
+	});
 
-    it('Should not be able to update the comment because there is no text', async() => {
-        const response = await request
-            .put(`/comment/update/${comment.id}`)
-            .send({
-                text: '',
-            });
+	it('Should not be able to update the comment because there is no text', async() => {
+		const response = await request
+			.put(`/comment/update/${comment.id}`)
+			.send({
+				text: '',
+			});
 
-            expect(response.status).toBe(400);
-    });
+		expect(response.status).toBe(400);
+	});
 
-    it('Should be able to delete the comment', async() => {
-        const response = await request.delete(`/comment/delete/${comment.id}`);
+	it('Should be able to delete the comment', async() => {
+		const response = await request.delete(`/comment/delete/${comment.id}`);
 
-        expect(response.status).toBe(200);
-    });
+		expect(response.status).toBe(200);
+	});
 
-    it('Should be able to like the comment', async() => {
-        const response = await request.post(`/comment/like/${comment.id}`);
+	it('Should be able to like the comment', async() => {
+		const response = await request.post(`/comment/like/${comment.id}`);
 
-        expect(response.status).toBe(200);
-    });
+		expect(response.status).toBe(200);
+	});
 
-    it('Should be able to dislike the comment', async() => {
-        const response = await request.post(`/comment/dislike/${comment.id}`);
+	it('Should be able to dislike the comment', async() => {
+		const response = await request.post(`/comment/dislike/${comment.id}`);
 
-        expect(response.status).toBe(200);
-    });
+		expect(response.status).toBe(200);
+	});
 });
--- src/controller/CommentController.js
+++ src/controller/CommentController.js
@@ -2,97 +2,97 @@ const Topic = require('../models/Topic');
 const Comment = require('../models/Comment');
 
 class CommentController{
-    async createComment(req, res){
-        try {
-            if (!req.body.text)
-              return res.status(400).send({ error: 'Comment should not be empty' });
-        
-            const comment = await Comment.create({
-              ...req.body,
-              user: req.params.userId,
-              topic: req.params.topicId,
-            });
-            const topic = await Topic.findById(req.params.topicId);
-        
-            await comment.save();
-        
-            topic.comments.push(comment);
-            await topic.save();
-        
-            return res.status(200).send({ message: 'Comment successfully registered.' });
-          } catch (err) {
-            return res.status(400).send({ error: `Error while commenting.${err}` });
-          }  
+  async createComment(req, res){
+    try {
+      if (!req.body.text)
+        return res.status(400).send({ error: 'Comment should not be empty' });
+  
+      const comment = await Comment.create({
+        ...req.body,
+        user: req.params.userId,
+        topic: req.params.topicId,
+      });
+      const topic = await Topic.findById(req.params.topicId);
+  
+      await comment.save();
+  
+      topic.comments.push(comment);
+      await topic.save();
+  
+      return res.status(200).send({ message: 'Comment successfully registered.' });
+      } catch (err) {
+        return res.status(400).send({ error: `Error while commenting.${err}` });
+      }  
     }
 
     async updateComment(req, res){
-        try {
-            await Comment.findById(req.params.commentId);
-            const newData = req.body;
-        
-            if (!newData.text)
-              return res.status(400).send({ error: 'Comment should not be empty' });
-        
-            await Comment.findOneAndUpdate({ _id: req.params.commentId }, req.body, {
-              useFindAndModify: false,
-            });
-            return res.statue(200).send({ message: 'Comment updated successfully.' });
-          } catch (err) {
-            return res
-              .status(400)
-              .send({ error: `Error while updating comment.${err}` });
-          }
+      try {
+        await Comment.findById(req.params.commentId);
+        const newData = req.body;
+    
+        if (!newData.text)
+          return res.status(400).send({ error: 'Comment should not be empty' });
+    
+        await Comment.findOneAndUpdate({ _id: req.params.commentId }, req.body, {
+          useFindAndModify: false,
+        });
+        return res.statue(200).send({ message: 'Comment updated successfully.' });
+      } catch (err) {
+        return res
+          .status(400)
+          .send({ error: `Error while updating comment.${err}` });
+      }
     }
 
     async deleteComment(req, res){
-        try {
-            const comment = await Comment.findById(req.params.commentId);
-            const topic = await Topic.findById(comment.topic);
-        
-            const index = topic.comments.indexOf(req.params.commentId);
-        
-            if (index > -1) {
-              topic.comments.splice(index, 1);
-            }
-        
-            topic.save();
-        
-            await Comment.findByIdAndRemove(req.params.commentId).populate('user');
-        
-            return res.status(200).send({
-              message: 'Comment successfully removed.',
-            });
-        } catch (err) {
-            return res.status(400).send({ error: `Error while deleting topic.${err}` });
+      try {
+        const comment = await Comment.findById(req.params.commentId);
+        const topic = await Topic.findById(comment.topic);
+    
+        const index = topic.comments.indexOf(req.params.commentId);
+    
+        if (index > -1) {
+          topic.comments.splice(index, 1);
         }
+    
+        topic.save();
+    
+        await Comment.findByIdAndRemove(req.params.commentId).populate('user');
+    
+        return res.status(200).send({
+          message: 'Comment successfully removed.',
+        });
+      } catch (err) {
+        return res.status(400).send({ error: `Error while deleting topic.${err}` });
+      }
     }
 
     async likeComment(req, res){
-        try {
-            await Comment.findOneAndUpdate(
-              { _id: req.params.commentId },
-              { $inc: { likes: 1 } },
-              { useFindAndModify: false }
-            );
-            return res.status(200).send({ message: 'Liked!' });
-        } catch (err) {
-            return res.status(400).send({ error: `Error while liking comment.${err}` });
-        }
+      try {
+        await Comment.findOneAndUpdate(
+          { _id: req.params.commentId },
+          { $inc: { likes: 1 } },
+          { useFindAndModify: false }
+        );
+        return res.status(200).send({ message: 'Liked!' });
+      } catch (err) {
+        return res.status(400).send({ error: `Error while liking comment.${err}` });
+      }
     }
 
     async dislikeComment(req, res){
-        try {
-            await Comment.findOneAndUpdate(
-              { _id: req.params.commentId },
-              { $inc: { dislikes: 1 } },
-              { useFindAndModify: false }
-            );
-            return res.status(200).send({ message: 'Disliked!' });
-        } catch (err) {
-            return res
-              .status(400)
-              .send({ error: `Error while linking comment.${err}` });
-        }
+      try {
+        await Comment.findOneAndUpdate(
+          { _id: req.params.commentId },
+          { $inc: { dislikes: 1 } },
+          { useFindAndModify: false }
+        );
+        return res.status(200).send({ message: 'Disliked!' });
+      } catch (err) {
+        return res
+          .status(400)
+          .send({ error: `Error while linking comment.${err}` });
+      }
     }
 }
 
