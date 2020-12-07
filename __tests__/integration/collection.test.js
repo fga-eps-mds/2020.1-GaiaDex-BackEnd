29ad5869dae778f3c51d05271783c73c9b4ae2dc
Victor Samuel <victor.samuelsantoss@gmail.com>
2020-12-07 18:41:44 -0300

    creation of tests on the collection route

---
+++ __tests__/integration/collection.test.js
@@ -0,0 +1,5 @@
+const supertest = require('supertest');
+const app = require('../../src/app');
+const { Topic: TopicModel } = require('../../src/models/Topic');
+const UserModel = require('../../src/models/User');
+
