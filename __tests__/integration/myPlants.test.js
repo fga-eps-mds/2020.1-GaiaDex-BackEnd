c01ceb4f2a0b8fcd1df1245eed65cc6e35168256
Victor Samuel <victor.samuelsantoss@gmail.com>
2020-12-07 20:38:10 -0300

    creating route tests for my plants

---
+++ __tests__/integration/myPlants.test.js
@@ -0,0 +1,23 @@
+const supertest = require('supertest');
+const app = require('../../src/app');
+const UserModel = require('../../src/models/User');
+const PlantModel = require('../../src/models/Plant');
+const { defaultUser2, defaultPlant1 } = require('../defaultModels');
+
+const request = supertest(app);
+
+let user;
+let plant;
+
+describe ('collection/', () => {
+    beforeEach(async (done) => {
+        user = new UserModel(defaultUser2);
+        await user.save();
+    
+        plant = new PlantModel(defaultPlant1);
+        await plant.save();
+        done();
+    });
+
+
+});
