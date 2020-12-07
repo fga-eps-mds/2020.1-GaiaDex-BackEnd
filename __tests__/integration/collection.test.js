57771805245d44c35e3ed47391e356830f936336
Victor Samuel <victor.samuelsantoss@gmail.com>
2020-12-07 19:17:57 -0300

    starting tests on the collection route

--- __tests__/integration/collection.test.js
+++ __tests__/integration/collection.test.js
@@ -1,5 +1,36 @@
 const supertest = require('supertest');
 const app = require('../../src/app');
-const { Topic: TopicModel } = require('../../src/models/Topic');
 const UserModel = require('../../src/models/User');
+const PlantModel = require('../../src/models/Plant');
+const { defaultUser2, defaultPlant1 } = require('../defaultModels');
 
+const request = supertest(app);
+
+let user;
+let plant;
+
+describe ('collection/', () => {
+    beforeEach(async (done) => {
+        user = new UserModel(defaultUser2);
+        await user.save();
+    
+        plant = new PlantModel(defaultPlant1);
+        await plant.save();
+        done();
+    });
+
+    // listing 
+    it('It should be possible to list a user collection.', async () => {
+        const response = await request.get(`/collection/${user.id}/`);
+        expect(response.status).toBe(200);
+    });
+
+    it('It not should be possible to list a user collection.', async () => {
+        const response = await request.get(`/collection/`);
+        expect(response.status).toBe(404);
+    });
+
+
+
+
+});
