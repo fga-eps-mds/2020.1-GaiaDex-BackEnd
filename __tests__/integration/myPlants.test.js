10399df07694e3d5e30a10c82712e9f5bdf4aacd
Victor Samuel <victor.samuelsantoss@gmail.com>
2020-12-07 21:17:37 -0300

    creating tests for editing my plant

--- __tests__/integration/myPlants.test.js
+++ __tests__/integration/myPlants.test.js
@@ -3,6 +3,7 @@ const app = require('../../src/app');
 const UserModel = require('../../src/models/User');
 const PlantModel = require('../../src/models/Plant');
 const { defaultUser2, defaultPlant1 } = require('../defaultModels');
+const myPlantSchema = require('../../src/schemas/myPlantSchema')
 
 const request = supertest(app);
 
@@ -41,4 +42,39 @@ describe ('collection/', () => {
         expect(response.status).toBe(404);
     });
 
+    // editing
+    it('It must be possible to edit the nickname of a particular plant.', async () => {
+        const response = await request.put(`/myPlants/edit/${plant.id}`)
+        .send({
+            nickname: 'newName',
+        });
+        const result = myPlantSchema
+            .validate({ nickname: response.nickname });
+        if (!result.error) 
+            expect(response.status).toBe(200);
+    });
+
+    it('It not must be possible to edit the nickname of a particular plant.', async () => {
+        const response = await request.put(`/myPlants/edit/${plant.id}`)
+        .send({
+            nickname: 'A',
+        });
+        const result = myPlantSchema
+            .validate({ nickname: response.nickname });
+        if (result.error) 
+            expect(response.status).toBe(400);
+    });
+
+    it('It not must be possible to edit the nickname of a particular plant.', async () => {
+        const response = await request.put(`/myPlants/edit/${plant.id}`);
+        expect(response.status).toBe(400);
+    });
+
+    it('It not must be possible to edit the nickname of a particular plant.', async () => {
+        const response = await request.put(`/myPlants/edit/${!plant.id}`)
+        .send({
+            nickname: 'newName',
+        });
+        expect(response.status).toBe(400);
+    });
 });
