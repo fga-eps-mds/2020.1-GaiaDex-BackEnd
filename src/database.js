cfa674ad4810132fa06c86bf7550c0a7cdb39f49
Rafael Makaha <rafael.makaha@gmail.com>
2020-12-14 00:03:33 -0300

    Removes unused comments from src/database.js file
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>
    Co-authored-by: Marcos Filipe <marofelipe@live.com>
    Co-authored-by: Antonio Ruan <ruanmoura13@outlook.com>
    Co-authored-by: João Vítor Morandi <joao.lemos4000@gmail.com>

--- src/database.js
+++ src/database.js
@@ -1,23 +1,5 @@
 const mongoose = require('mongoose');
 
-// MongoDB connection
-// mongodb://localhost:27017/noderest  => meu banco de dados local polupado
-// mongodb://mongo:27017/backend => banco de dados da develop
-// mongoose
-//   .connect('mongodb://mongo:27017/backend', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log('MongoDB Connected'))
-//   .catch((err) => console.log(err));
-// console.log(`db host ${process.env.DB_HOST}`);
-// console.log(`db host ${process.env.DB_PORT}`);
-// console.log(`db host ${process.env.DB_NAME}`);
-// console.log(
-//   `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-// );
-// Pra subir pra produção: `mongo://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-
 const connect = async () => {
   if (mongoose.connection.readyState === 0) {
     let url;
