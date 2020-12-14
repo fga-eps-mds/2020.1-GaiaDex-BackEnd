119335f738ed35bdb39ad8806fb3f8f2d6b65b82
Rafael Makaha <rafael.makaha@gmail.com>
2020-12-14 00:05:28 -0300

    Fix homolog and production database connection.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>
    Co-authored-by: Marcos Filipe <marofelipe@live.com>
    Co-authored-by: Antonio Ruan <ruanmoura13@outlook.com>
    Co-authored-by: João Vítor Morandi <joao.lemos4000@gmail.com>

--- src/database.js
+++ src/database.js
@@ -4,8 +4,11 @@ const connect = async () => {
   if (mongoose.connection.readyState === 0) {
     let url;
     switch (process.env.NODE_ENV) {
+      case 'homolog':
+        url = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`;
+        break;
       case 'production':
-        url = `mongo://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+        url = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`;
         break;
       case 'test':
         url = global.__DB_URL__;
@@ -13,13 +16,29 @@ const connect = async () => {
       default:
         url = `mongodb://mongo:27017/backend`;
     }
-
-    await mongoose.connect(url, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    });
+    if (
+      process.env.NODE_ENV === 'homolog' ||
+      process.env.NODE_ENV === 'production'
+    ) {
+      await mongoose
+        .connect(url, {
+          useNewUrlParser: true,
+          useCreateIndex: true,
+          useUnifiedTopology: true,
+        })
+        .then(() => console.log('MongoDB Connected'))
+        .catch((err) => console.log(err));
+    } else {
+      await mongoose
+        .connect(url, {
+          useNewUrlParser: true,
+          useCreateIndex: true,
+          useFindAndModify: false,
+          useUnifiedTopology: true,
+        })
+        .then(() => console.log('MongoDB Connected'))
+        .catch((err) => console.log(err));
+    }
   }
 };
 
